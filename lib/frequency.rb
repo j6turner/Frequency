569ec00375d4c930e58c5e51cf65c16ff950b512
J. TURNER <student@epicodus.com>
2015-01-11 23:43:07 -0800

    Add README.

--- lib/frequency.rb
+++ lib/frequency.rb
@@ -1,11 +1,10 @@
 define_method(:frequency) do |word|
   words = self.split()
-  wf = Hash.new(0)
-  words.each { |word| wf[word] += 1 }
-  wf = wf.values.sort
-    if words.include?(word)
-      word
-    else
-      "No luck ... please enter a word included in the phrase."
+  x = 0
+  freq = Hash.new(x)
+  words.each() do |word|
+  if words.include?(word)
+    x = x + 1
   end
+  x
 end
--- spec/frequency_spec.rb
+++ spec/frequency_spec.rb
@@ -3,13 +3,19 @@ require('facets')
 require('frequency')
 
 describe('String#frequency') do
-  it("returns instruction if the submitted word does not appear in the phrase") do
-    expect(("to be or not to be").frequency("fire")).to (eq("No luck ... please enter a word included in the phrase."))
+  it("counts whether given word appears in one-word string") do
+    expect("be".frequency("be")).to(eq(1))
   end
 end
 
 describe('String#frequency') do
   it("returns the number of times submitted word appears in the phrase") do
-    expect(("to be or not to be").frequency("be")).to (eq(2))
+    expect("to be or not to be".frequency("be")).to(eq(2))
+  end
+end
+
+describe('String#frequency') do
+  it("returns instruction if the submitted word does not appear in the phrase") do
+    expect("to be or not to be".frequency("fire")).to(eq("No luck ... please enter a word included in the phrase."))
   end
 end
